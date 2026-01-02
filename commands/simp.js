7930d84063579a37e2f2f3a6123ec28c160b6775
travelsok5-arch <travelsok5@gmail.com>
2026-01-02 21:23:46 +0500

    Change newsletter name from 'KnightBot MD' to 'Hassi Tarar Bot'

--- commands/simp.js
+++ commands/simp.js
@@ -38,7 +38,7 @@ async function simpCommand(sock, chatId, quotedMsg, mentionedJid, sender) {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterName: 'Hassi Tarar Bot',
                     serverMessageId: -1
                 }
             }
@@ -53,7 +53,7 @@ async function simpCommand(sock, chatId, quotedMsg, mentionedJid, sender) {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterName: 'Hassi Tarar Bot',
                     serverMessageId: -1
                 }
             }
