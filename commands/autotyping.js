ba6a3433baa94adf4df41ab620e3fe7a91290350
travelsok5-arch <travelsok5@gmail.com>
2026-01-02 21:11:52 +0500

    Update newsletter name from 'KnightBot MD' to 'Hassi Tarar Bot'

--- commands/autotyping.js
+++ commands/autotyping.js
@@ -32,7 +32,7 @@ async function autotypingCommand(sock, chatId, message) {
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
                         newsletterJid: '120363161513685998@newsletter',
-                        newsletterName: 'KnightBot MD',
+                        newsletterName: 'Hassi Tarar Bot',
                         serverMessageId: -1
                     }
                 }
